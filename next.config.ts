7fa174583ff00fd7900cf8f9afdebf9eb430f5e4
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2025-06-12 10:51:56 +0330

    Change nextcofig to solve error for images on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,31 @@
 import type { NextConfig } from 'next';
 
+// const nextConfig: NextConfig = {
+//     /* config options here */
+//     images: {
+//         remotePatterns: [
+//             {
+//                 protocol: 'https',
+//                 hostname: 'randomuser.me',
+//                 port: '',
+//                 pathname: '/api/portraits/**'
+//             },
+//             {
+//                 protocol: 'https',
+//                 hostname: 'flagcdn.com',
+//                 port: '',
+//                 pathname: '/**'
+//             }
+//         ]
+//     }
+// };
+
 const nextConfig: NextConfig = {
-    /* config options here */
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'randomuser.me',
-                port: '',
-                pathname: '/api/portraits/**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'flagcdn.com',
-                port: '',
-                pathname: '/**'
-            }
-        ]
+        domains: ['randomuser.me', 'flagcdn.com'],
+        unoptimized: true,
     }
 };
 
 export default nextConfig;
+
