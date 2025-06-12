d7a6202b0e48651f191c33762fb77dbd69e735b9
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2025-06-12 10:32:57 +0330

    Change profile page tile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,21 +1,9 @@
 import ProfilePage from '@/features/profile/components/ProfilePage';
 import { Metadata } from 'next';
 
-type Props = {
-    searchParams: { [key: string]: string | string[] | undefined };
+export const metadata: Metadata = {
+    title: 'Profile',
 };
-
-
-export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
-    const params = await searchParams; // ✅ Await searchParams
-    const nameParam = params['name'];
-    const name = Array.isArray(nameParam) ? nameParam[0] : nameParam;
-
-    return {
-        title: `${name || 'User'}'s Profile`,
-    };
-}
-
 
 
 const Page = () => {
