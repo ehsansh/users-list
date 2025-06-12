2620faa424ca3b7be1c1c0446b203808a22fe9a2
Ehsan Shadmehri <ehsanshadmehri@gmail.com>
2025-06-12 11:27:40 +0330

    Use enviroment variables in next.config.ts file and avoid hard coding domain names

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     images: {
-        domains: ['randomuser.me', 'flagcdn.com'],
+        domains: [
+            new URL(process.env.NEXT_PUBLIC_API_BASE_URL!).hostname,
+            new URL(process.env.NEXT_PUBLIC_FLAG_BASE_URL!).hostname
+        ],
         unoptimized: true,
     }
 };
